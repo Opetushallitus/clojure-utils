5970ed8c07694afc08ed29681815b1925da862c5
Mikko Suonio <mikko.suonio@solita.fi>
2015-04-22 14:31:14 +0300

    Lisää tietokannan luonti

--- src/clj/oph/korma/common.clj
+++ src/clj/oph/korma/common.clj
@@ -1,10 +1,38 @@
 (ns oph.korma.common
   (:import java.sql.Date
+           com.jolbox.bonecp.BoneCPDataSource
            org.joda.time.LocalDate)
   (:require  korma.db
              [korma.core :as sql]
              [clj-time.coerce :as time-coerce]
              [clj-time.core :as time]))
+
+(defn korma-asetukset
+  "Muuttaa asetustiedoston db-avaimen arvon Korman odottamaan muotoon."
+  [db-asetukset]
+  (clojure.set/rename-keys db-asetukset {:name :db}))
+
+(defn bonecp-datasource
+  "BoneCP based connection pool"
+  [db-asetukset]
+  (let [korma-postgres (korma.db/postgres (korma-asetukset db-asetukset))
+        bonecp-ds (doto (com.jolbox.bonecp.BoneCPDataSource.)
+                    (.setJdbcUrl (str "jdbc:" (:subprotocol korma-postgres) ":" (:subname korma-postgres)))
+                    (.setUsername (:user korma-postgres))
+                    (.setPassword (:password korma-postgres))
+                    (.setConnectionTestStatement "select 42")
+                    (.setConnectionTimeoutInMs 2000)
+                    (.setDefaultAutoCommit false)
+                    (.setMaxConnectionsPerPartition 10)
+                    (.setMinConnectionsPerPartition 5)
+                    (.setPartitionCount 1))]
+    bonecp-ds))
+
+(defn luo-db [db-asetukset]
+  (korma.db/default-connection
+    (korma.db/create-db {:make-pool? false
+                         :delimiters ""
+                         :datasource (bonecp-datasource db-asetukset)})))
 
 (defn convert-instances-of [c f m]
   (clojure.walk/postwalk #(if (instance? c %) (f %) %) m))
