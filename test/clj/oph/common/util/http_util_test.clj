179eb33cc4a1c838605a50853d1de09ecab729fa
Antti Virtanen <antti.virtanen@iki.fi>
2015-11-04 09:05:36 +0200

    Korjattu json-response-nocache funktion toteutus. Yksikkötesti korjatulle funktiolle.

--- test/clj/oph/common/util/http_util_test.clj
+++ test/clj/oph/common/util/http_util_test.clj
@@ -84,4 +84,11 @@
     (testing "palauttaa vastauksen sarjallisetettuna ei-nil-syötteellä"
       (let [data {:foo "Bar"}]
         (is (= (:body (json-response data))
-               "{\"foo\":\"Bar\"}"))))))
+               "{\"foo\":\"Bar\"}"))))
+    
+    (testing "no-cache json toimii oikein"
+       (let [data {:kung :fury}]
+         (is (= (get (:headers (json-response-nocache data)) "Cache-control")
+                "max-age=0"))))))
+
+
