d0c02e4f91b5cd7abbf592436adc9c3bd239d0da
Jari Asp <jari.asp@solita.fi>
2017-08-08 13:08:22 +0300

    OPH-1966: audit-logitus (OPH:n projektien yhteinen)

---
+++ src/clj/oph/common/infra/common_audit_log.clj
@@ -0,0 +1,157 @@
+;; Copyright (c) 2013 The Finnish National Board of Education - Opetushallitus
+;;
+;; This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+;; soon as they will be approved by the European Commission - subsequent versions
+;; of the EUPL (the "Licence");
+;;
+;; You may not use this work except in compliance with the Licence.
+;; You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; European Union Public Licence for more details.
+
+(ns oph.common.infra.common-audit-log
+  "Yhteinen audit-lokituksen abstrahoiva rajapinta."
+  (:require
+    [schema.core :as s]
+    [cheshire.core :as cheshire]
+    [cheshire.generate :as json-gen]
+    [clj-time.core :as time]
+    [clj-time.local :as time-local]
+    ))
+
+
+;; Esimerkkejä:
+;
+;{"logSeq":"21785","bootTime":"2017-06-07T16:06:47.613+03","hostname":"konsepti","timestamp":"2017-06-27T09:06:49.002+03","serviceName":"tarjonta","applicationType":"virkailija","message":"Alive!"}
+;{"logSeq":"21786","bootTime":"2017-06-07T16:06:47.613+03","hostname":"konsepti","timestamp":"2017-06-27T09:06:56.913+03","serviceName":"tarjonta","applicationType":"virkailija","resource":"HAKUKOHDE","resourceOid":"1.2.246.562.20.20344157233","id":"1.2.246.562.24.69880686397","operaatio":"PUBLISH"}
+;{"logSeq":"21787","bootTime":"2017-06-07T16:06:47.613+03","hostname":"konsepti","timestamp":"2017-06-27T09:10:06.001+03","serviceName":"tarjonta","applicationType":"virkailija","resource":"KOULUTUS","delta":"[{\"op\":\"replace\",\"path\":\"/modified\",\"value\":1498543804238},{\"op\":\"replace\",\"path\":\"/version\",\"value\":1},{\"op\":\"replace\",\"path\":\"/tila\",\"value\":\"VALMIS\"}]","resourceOid":"1.2.246.562.17.22855189217","id":"1.2.246.562.24.89924410056","operaatio":"UPDATE"}
+
+;; Ylläolevat avattuna:
+;
+;{"logSeq":"21785",
+; "bootTime":"2017-06-07T16:06:47.613+03",
+; "hostname":"konsepti",
+; "timestamp":"2017-06-27T09:06:49.002+03",
+; "serviceName":"tarjonta",
+; "applicationType":"virkailija",
+; "message":"Alive!"}
+;
+;{"logSeq":"21786",
+; "bootTime":"2017-06-07T16:06:47.613+03",
+; "hostname":"konsepti",
+; "timestamp":"2017-06-27T09:06:56.913+03",
+; "serviceName":"tarjonta",
+; "applicationType":"virkailija",
+; "resource":"HAKUKOHDE",
+; "resourceOid":"1.2.246.562.20.20344157233",
+; "id":"1.2.246.562.24.69880686397",
+; "operaatio":"PUBLISH"}
+;
+;{"logSeq":"21787",
+; "bootTime":"2017-06-07T16:06:47.613+03",
+; "hostname":"konsepti",
+; "timestamp":"2017-06-27T09:10:06.001+03",
+; "serviceName":"tarjonta",
+; "applicationType":"virkailija",
+; "resource":"KOULUTUS",
+; "delta":"[{\"op\":\"replace\",
+;            \"path\":\"/modified\",
+;            \"value\":1498543804238},
+;           {\"op\":\"replace\",
+;            \"path\":\"/version\",
+;            \"value\":1},
+;           {\"op\":\"replace\",
+;            \"path\":\"/tila\",
+;            \"value\":\"VALMIS\"}]",
+; "resourceOid":"1.2.246.562.17.22855189217",
+; "id": "1.2.246.562.24.89924410056",
+; "operaatio":"UPDATE"}
+
+(s/defschema Env-meta {:boot-time        s/Any   ;; TODO: Tarkasta datetime
+                       :hostname         s/Str
+                       :service-name     s/Str
+                       :application-type (s/enum "oppija" "virkailija")})
+
+(s/defschema Logientry {:operation (s/enum :kirjautuminen :lisays :paivitys :poisto)   ;; Huom: esimerkeissä avaimena "operaatio"
+                        :user {:oid        s/Str
+                               :ip         s/Str
+                               :session    s/Str
+                               :user-agent s/Str}
+                        :resource s/Str
+                        :resourceOid (s/maybe s/Str)
+                        :id s/Str  ;; pääavain
+                        (s/optional-key :delta) [{:op (s/enum :lisays :paivitys :poisto)
+                                                  :path s/Str
+                                                  :value s/Any}]
+                        (s/optional-key :message) s/Str})
+
+(def ^:private version  1)
+(def ^:private type-log "log")  ;; Meillä ei tueta "alive"-logiviestejä
+(def ^:private log-seq  (atom (bigint 0)))
+(def environment-meta   (atom {:boot-time        nil
+                               :hostname         nil
+                               :service-name     nil
+                               :application-type nil}))
+
+(def operaatiot {:kirjautuminen "kirjautuminen"
+                 :lisays "lisäys"
+                 :paivitys "päivitys"
+                 :poisto "poisto"})
+
+(json-gen/add-encoder org.joda.time.DateTime
+                      (fn [c json-generator]
+                        (.writeString json-generator (.toString c))))
+(json-gen/add-encoder org.joda.time.LocalDateTime                                    ;; TODO: Tarvitaanko tätä?
+                      (fn [c json-generator]
+                        (.writeString json-generator (.toString c))))
+(json-gen/add-encoder org.joda.time.LocalDate
+                      (fn [c json-generator]
+                        (.writeString json-generator (.toString c "dd.MM.yyyy"))))    ;; TODO: Onko muoto "dd.MM.yyyy" ok, vai printataanko samoin kuin kuut yllä?
+
+(defn log [log-contents]
+  {:pre [#_(every? (comp not nil?) [boot-time
+                                   hostname
+                                   serviceName
+                                   applicationType])
+;         (every?
+;           (fn [[k v]] (not (nil? v)))
+;           @environment-meta)
+;         (nil? (s/check Logientry log-contents))
+         ]}
+  ;; pidä nämä alussa
+  (s/validate Env-meta  @environment-meta)
+  (s/validate Logientry log-contents)
+
+  (reset! log-seq (inc @log-seq))
+  (let [json-contents (cheshire/generate-string (merge
+                                                  {:version         version
+                                                   :logSeq          @log-seq
+                                                   :type            type-log
+                                                   :bootTime        (:boot-time @environment-meta)
+                                                   :hostname        (:hostname @environment-meta)
+                                                   :timestamp       (time-local/local-now)              ;; TODO: Onko tämä ok?
+                                                   :serviceName     (:service-name @environment-meta)
+                                                   :applicationType (:application-type @environment-meta)
+                                                   :operation       (get operaatiot (:operation log-contents))
+                                                   :user            {:oid       (-> log-contents :user :oid)
+                                                                     :ip        (-> log-contents :user :ip)
+                                                                     :session   (-> log-contents :user :session)
+                                                                     :userAgent (-> log-contents :user :user-agent)}
+                                                   }
+                                                  (when (:delta log-contents)
+                                                    {:delta (:delta log-contents)})
+                                                  ;; TODO: Tarkastetaanko tässä :id:n löytyminen?
+                                                  (when (:resource log-contents) #_(and (:resource log-contents) (:resourceOid log-contents) (:id log-contents))
+                                                    {:target {(:resource log-contents) (:resourceOid log-contents)
+                                                              :id (:id log-contents)}})
+                                                  (when (:message log-contents)
+                                                    {:message (:message log-contents)})
+                                                  ))]
+    json-contents
+    ))
+
+
+
---
+++ test/clj/oph/common/infra/common_audit_log_test.clj
@@ -0,0 +1,59 @@
+(ns oph.common.infra.common-audit-log-test
+  (:require [clojure.test :refer [deftest testing is are]]
+            [cheshire.core :as json]
+            [clj-time.core :as time]
+            [clj-time.local :as time-local]
+            [oph.common.infra.common-audit-log :refer :all]))
+
+(def ^:private boot-time (time-local/local-now))   ;; (time/local-date-time 1980 9 20 1 2 3 123)
+(def ^:private validi-meta {:boot-time        boot-time
+                            :hostname         "host"
+                            :service-name     "aitu"
+                            :application-type "virkailija"})
+
+(deftest auditlogitus-test
+
+  (testing "environment metaa ei ole annettu"
+;    (is (thrown? AssertionError (log {})))
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Value does not match schema" (log {})))
+    )
+
+  (testing "environment meta on epävalidi"
+    (reset! environment-meta {:service-name "aitu"})
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Value does not match schema" (log {})))
+    )
+
+  (testing "meta annettu mutta logisisältö puuttuu"
+    (reset! environment-meta validi-meta)
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Value does not match schema" (log {})))
+    )
+
+  (testing "kaikki kentät annettu"
+    (reset! environment-meta validi-meta)
+    (let [resp (log {:operation :paivitys
+                     :user {:oid        "henkiloOid"
+                            :ip         "127.0.0.1"
+                            :session    "124uih23u124"
+                            :user-agent "Apache-HttpClient/4.5.2(Java/1.8.0_121)"}
+                     :resource "järjestämissopimus"
+                     :resourceOid "sopimusId"
+                     :id "id"
+                     :delta [{:op :paivitys
+                              :path "alkupvm"
+                              :value (time/local-date 2009 8 1)}
+                             {:op :paivitys
+                              :path "loppupvm"
+                              :value (time/local-date 2009 7 31)}]
+                     :message "Tämä on viesti."})
+          ]
+      ;; Ohitetaan muuttuvan "timestamp"-arvon tarkastelu.
+      (is (and
+            (.contains resp
+              "\"operation\":\"päivitys\",\"type\":\"log\",\"hostname\":\"host\",\"applicationType\":\"virkailija\",\"delta\":[{\"op\":\"paivitys\",\"path\":\"alkupvm\",\"value\":\"01.08.2009\"},{\"op\":\"paivitys\",\"path\":\"loppupvm\",\"value\":\"31.07.2009\"}],\"logSeq\":1")
+            (.contains resp
+              "\"target\":{\"järjestämissopimus\":\"sopimusId\",\"id\":\"id\"},\"serviceName\":\"aitu\",\"version\":1")
+            (.contains resp
+              "\"user\":{\"oid\":\"henkiloOid\",\"ip\":\"127.0.0.1\",\"session\":\"124uih23u124\",\"userAgent\":\"Apache-HttpClient/4.5.2(Java/1.8.0_121)\"},\"message\":\"Tämä on viesti.\"")
+            (.contains resp (json/generate-string boot-time))))
+      ))
+  )
